0ff3d94d5eb40d527540d9e9aac23f129992dd2a
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-05-31 19:15:33 +0200

    Ho creato tutto il layout con la logica dei tasti.

--- src/components/elements/WordRight.jsx
+++ src/components/elements/WordRight.jsx
@@ -1,3 +1,34 @@
-export default function WordRight() {
-  return;
+export default function WordRight({ idCharClicked, secretWord }) {
+  const secretWordArray = [
+    {
+      id: 1,
+      char: "r",
+      revealed: false,
+      display: "_",
+    },
+  ];
+
+  for (let i = 0; i < secretWord.length; i++) {
+    secretWordArray[i] = {
+      id: i,
+      char: secretWord[i],
+      revealed: false,
+      display: "_",
+    };
+  }
+
+  console.log(secretWordArray);
+  return (
+    <div className="container">
+      <div className="row d-flex display-justify-around text-white">
+        {secretWordArray.map((char) => {
+          return (
+            <div className="col" key={char.id}>
+              {char.display}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
